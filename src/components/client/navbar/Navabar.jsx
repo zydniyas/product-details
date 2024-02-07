28a77e970803cdc81956ed2da53b753656dab30e
zydniyas <94290366+zydniyas@users.noreply.github.com>
2024-02-07 22:44:35 +0530

    product details page design

--- src/components/client/navbar/Navabar.jsx
+++ src/components/client/navbar/Navabar.jsx
@@ -3,67 +3,95 @@ import { useNavigate } from "react-router-dom";
 import { UserContext } from "../../../App";
 
 const navItems = [
-	{ item: "home", link: "/" },
-	{ item: "about", link: "/about" },
-	{ item: "contact", link: "/contact" },
-	{ item: "support", link: "/support" },
-	{ item: "products", link: "/products" },
+  { item: "home", link: "/" },
+  { item: "about", link: "/about" },
+  { item: "contact", link: "/contact" },
+  { item: "support", link: "/support" },
+  { item: "products", link: "/products" },
 ];
 
 function Navabar() {
-	const { userDetails, setUserDetails } = useContext(UserContext);
-	const userDetail = { username: "niyas", avatar: "" };
-	const [loading, setLoading] = useState(false);
-	const navigate = useNavigate();
-	function login() {
-		setLoading(true);
-		setTimeout(() => {
-			setUserDetails(userDetail);
-			setLoading(false);
-		}, 5000);
-	}
-	return (
-		<div className="bg-black z-50 flex justify-between sticky top-0 p-4 items-center">
-			<h1 className="text-2xl uppercase font-extrabold text-white cursor-pointer hover:scale-105 duration-300">
-				logo
-			</h1>
-			<div className="flex gap-4">
-				<ul className="flex justify-center items-center gap-4">
-					{navItems.map((elem, i) => {
-						return (
-							<li
-								key={i}
-								onClick={() => navigate(elem.link)}
-								className="uppercase text-white font-bold cursor-pointer hover:scale-105 duration-300"
-							>
-								{elem.item}
-							</li>
-						);
-					})}
-				</ul>
-				{userDetails ? (
-					<div
-						onClick={() => setUserDetails(null)}
-						className={`${
-							loading ? "hidden" : "w-10 h-10 bg-white rounded-full cursor-pointer"
-						} `}
-					></div>
-				) : (
-					<button
-						onClick={() => login()}
-						className={`${
-							loading
-								? "hidden"
-								: "bg-white rounded px-6 py-2 uppercase font-bold hover:bg-black border hover:border-white hover:text-white duration-300"
-						} `}
-					>
-						login
-					</button>
-				)}
-				{loading && <div className="w-10 h-10 animate-spin bg-white"></div>}
-			</div>
-		</div>
-	);
+  const { userDetails, setUserDetails } = useContext(UserContext);
+  const userDetail = { username: "niyas", avatar: "" };
+  const [loading, setLoading] = useState(false);
+  const navigate = useNavigate();
+  function login() {
+    setLoading(true);
+    setTimeout(() => {
+      setUserDetails(userDetail);
+      setLoading(false);
+    }, 5000);
+  }
+  return (
+    <header>
+      <nav
+        className="
+          flex flex-wrap
+          items-center
+          justify-between
+          w-full
+          py-4
+          md:py-0
+          px-4
+          text-lg text-white
+          bg-black
+        "
+      >
+        <div>
+          <a href="#"></a>
+        </div>
+
+        <div
+          className="hidden w-full md:flex md:items-center md:w-auto"
+          id="menu"
+        >
+          <ul
+            className="
+              pt-4
+              text-base text-white
+              md:flex
+              md:justify-between 
+              md:pt-0"
+          >
+            {navItems.map((elem, i) => {
+              return (
+                <li
+                  key={i}
+                  onClick={() => navigate(elem.link)}
+                  className="md:p-4 py-2 block hover:text-purple-400 cursor-pointer"
+                  href="#"
+                >
+                  {elem.item}
+                </li>
+              );
+            })}
+          </ul>
+          {userDetails ? (
+            <div
+              onClick={() => setUserDetails(null)}
+              className={`${
+                loading
+                  ? "hidden"
+                  : "w-10 h-10 bg-white rounded-full cursor-pointer"
+              } `}
+            ></div>
+          ) : (
+            <button
+              onClick={() => login()}
+              className={`${
+                loading
+                  ? "hidden"
+                  : "bg-white rounded px-6 py-2 uppercase font-bold hover:bg-black text-black border hover:border-white hover:text-white duration-300"
+              } `}
+            >
+              login
+            </button>
+          )}
+          {loading && <div className="w-10 h-10 animate-spin bg-white"></div>}
+        </div>
+      </nav>
+    </header>
+  );
 }
 
 export default Navabar;
--- src/components/client/productDetails/ProductDetails.jsx
+++ src/components/client/productDetails/ProductDetails.jsx
@@ -3,15 +3,103 @@ import { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
 
 function ProductDetails() {
-	const [product, setProduct] = useState(null);
-	const { id } = useParams();
+  const [product, setProduct] = useState(null);
+  const { id } = useParams();
 
-	useEffect(() => {
-		axios.get(`https://dummyjson.com/products/${id}`).then(res => {
-			setProduct(res.data);
-		});
-	}, [id]);
-	return <div>{product?.title}</div>;
+  useEffect(() => {
+    axios.get(`https://dummyjson.com/products/${id}`).then((res) => {
+      setProduct(res.data);
+    });
+  }, [id]);
+  return (
+    <div className="bg-gray-100 dark:bg-gray-800 py-8 h-screen">
+      <div className="max-w-6xl mx-auto px-4 sm:px-6 lg:px-8">
+        <div className="flex flex-col md:flex-row -mx-4">
+          <div className="md:flex-1 px-4">
+            <div className="h-[460px] rounded-lg bg-gray-300 dark:bg-gray-700 mb-4">
+              <img
+                className="w-full h-full object-contain"
+                src={product?.thumbnail}
+                alt="Product Image"
+              />
+            </div>
+            <div className="flex -mx-2 mb-4">
+              <div className="w-1/2 px-2">
+                <button className="w-full bg-gray-900 dark:bg-gray-600 text-white py-2 px-4 rounded-full font-bold hover:bg-gray-800 dark:hover:bg-gray-700">
+                  Add to Cart
+                </button>
+              </div>
+              <div className="w-1/2 px-2">
+                <button className="w-full bg-gray-200 dark:bg-gray-700 text-gray-800 dark:text-white py-2 px-4 rounded-full font-bold hover:bg-gray-300 dark:hover:bg-gray-600">
+                  Add to Wishlist
+                </button>
+              </div>
+            </div>
+          </div>
+          <div className="md:flex-1 px-4">
+            <h2 className="text-2xl font-bold text-gray-800 dark:text-white mb-2">
+              {product?.title}
+            </h2>
+            <p className="text-gray-600 dark:text-gray-300 text-sm mb-4">
+              {product?.brand}
+            </p>
+
+            <div className="flex flex-wrap mb-4 gap-3">
+              <div className="mr-4">
+                <span className="font-bold text-gray-700 dark:text-gray-300">
+                  Price:
+                </span>
+                <span className="text-gray-600 dark:text-gray-300">
+                  {product?.price}
+                </span>
+              </div>
+              <div className="mr-4">
+                <span className="font-bold text-gray-700 dark:text-gray-300">
+                  DiscountPercentage:
+                </span>
+                <span className="text-gray-600 dark:text-gray-300">
+                  {product?.discountPercentage}
+                </span>
+              </div>
+              <div>
+                <span className="font-bold text-gray-700 dark:text-gray-300">
+                  Availability:
+                </span>
+                <span className="text-gray-600 dark:text-gray-300">
+                  In Stock({product?.stock})
+                </span>
+              </div>
+              <div>
+                <span className="font-bold text-gray-700 dark:text-gray-300">
+                  Rating:
+                </span>
+                <span className="text-gray-600 dark:text-gray-300">
+                  {product?.rating}
+                </span>
+              </div>
+              <div>
+                <span className="font-bold text-gray-700 dark:text-gray-300">
+                  Category :
+                </span>
+                <span className="text-gray-600 dark:text-gray-300">
+                  {product?.category}
+                </span>
+              </div>
+            </div>
+
+            <div>
+              <span className="font-bold text-gray-700 dark:text-gray-300">
+                Product Description:
+              </span>
+              <p className="text-gray-600 dark:text-gray-300 text-sm mt-2">
+                {product?.description}
+              </p>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default ProductDetails;
